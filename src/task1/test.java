d6b548619e56c0b7b95ce9aa2f529d03f328e973
yin <yin_1214@163.com>
2021-08-18 11:58:16 +0800

    Commit Message

--- src/task1/test.java
+++ src/task1/test.java
@@ -1,4 +1,29 @@
 package task1;
+//idea快捷键
+
+        /*2021年8月16日
+        10:29*/
+
+     /*   1、创建内容：Alt+insert
+        2、main方法：psvm
+        3、输出语句：sout
+        4、复制一行：ctrl+d
+        5、删除一行：Ctrl+y
+        6、代码向上/下移动：Ctrl+shift+up/Down
+        7、搜索：Ctrl+n
+        8、生成代码：alt+insert(如构造函数等，getter，setter，hashCode，equals，toString)
+        9、百能快捷键：alt+Enter(导包，生成变量等)
+        10、单行注释或多行注释：Ctrl + /或Ctrl + Shift + /
+        11、重命名 shift+f6
+        12、for循环 直接：fori 回车
+        13、代码块包围：try-catch,if,while等 Ctrl+alt+t
+        14、代码自动补全提示：
+        15、idea代码字体大小放大和缩小的快捷键
+        16、代码一层一层调用的快捷键：
+        17、显示代码结构：alt+7
+        18、显示导航栏：alt+1
+        */
+
 
 public class test {
     public static void main(String[] args) {
